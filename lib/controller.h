7fb625bbf51e6fe4f8c593859043ceb3e8798fbe
Reggae Shark <clementcenier@gmail.com>
2022-11-01 13:45:21 +0100

    Add some fct signature to future work in controller.h

--- lib/controller.h
+++ lib/controller.h
@@ -6,4 +6,13 @@
 #ifndef PROJET_CONTROLLER_H
 #define PROJET_CONTROLLER_H
 
+// fct globale appelée apres chaque input d un joueur, qui renvoie la map avec des modifs (joueur qui bouge, bombe posée ...)
+Map action(Map map, char keyPressed, int player_ID);
+
+// petite fct qui renvoie l ID d un item si un item spawn
+int spawnItem();
+
+// call 1 fois apres la creation de la map
+Player initPlayer();
+
 #endif //PROJET_CONTROLLER_H
